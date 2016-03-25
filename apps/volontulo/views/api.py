db26986fb0e37672bc28cd61f05694d2fe431d89
Maciej Zięba <maciej.zieba@stxnext.pl>
2016-03-25 17:53:19 +0100

    Created new permission for creation of offers through REST API

--- apps/volontulo/views/api.py
+++ apps/volontulo/views/api.py
@@ -3,11 +3,28 @@
 u"""
 .. module:: api
 """
+from rest_framework import permissions
 from rest_framework import viewsets
 
 from apps.volontulo.models import Offer, Organization, UserProfile
 from apps.volontulo.serializers import (
     OfferSerializer, OrganizationSerializer, UserProfileSerializer)
+
+
+class UserOfferPermission(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        """
+        All users can list and retrieve offers
+        but only authenticated users can create new ones.
+        """
+        if view.action in ('list', 'retrieve'):
+            return True
+        elif view.action == 'create':
+            return (request.user.is_authenticated() and
+                    not request.user.userprofile.is_administrator)
+        else:
+            return False
 
 
 class UserProfileViewSet(viewsets.ReadOnlyModelViewSet):
@@ -18,12 +35,13 @@ class UserProfileViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = UserProfileSerializer
 
 
-class OfferViewSet(viewsets.ReadOnlyModelViewSet):
+class OfferViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows Offers to be viewed or edited.
     """
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
+    permission_classes = (UserOfferPermission, )
 
 
 class OrganizationViewSet(viewsets.ReadOnlyModelViewSet):
